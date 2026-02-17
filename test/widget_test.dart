9303a0d1748dbb9b35a159ba1e9d40e91367616a
V1taliy500 <staryk.vitaliy@outlook.com>
2026-02-17 23:26:22 +0200

    Add a 1 laboratory work

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:iot/main.dart';
+import 'package:iot_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
